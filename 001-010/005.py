62326377136edfc2a3c97bb9dd20d5a9b7c96970
phanvanhoang-dev <hoanggphann2409@gmail.com>
2026-01-07 23:06:29 +0700

    Add functions to calculate average and classify student

---
+++ 001-010/005.py
@@ -0,0 +1,14 @@
+def calculate_average(scores):
+	return sum(scores)/len(scores)
+def classify_student(average)
+	if average>=8.5:
+		return "Xuất sắc"
+	elif average>=7.0:
+		return "Giỏi"
+	elif average>=5.5:
+		return "Khá"
+	elif average>=4:
+		return "Trung bình"
+	else:
+		return "Yếu "
+
