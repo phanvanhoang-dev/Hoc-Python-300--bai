bb232c9a1f085906da886773832455f3fd33d7fc
phanvanhoang-dev <hoanggphann2409@gmail.com>
2026-01-11 23:16:28 +0700

    Update 007.py

--- 001-010/007.py
+++ 001-010/007.py
@@ -1,1 +1,10 @@
-
+def Kiem_tra_nam(year):
+  if year % 400 ==0 or year %4 ==0 and year % 100 !=0:
+    print("Đây là năm nhuận")
+  else:
+    print("Đây không phải là năm nhuận")
+try:
+  year = int(input("Nhập năm:"))
+  if year <0:
+    print("Vui lòng nhập năm sau công nguyên")
+    
