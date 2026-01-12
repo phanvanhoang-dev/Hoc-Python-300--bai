78b14b984f623dcf7370f84491c72aaca63451a0
phanvanhoang-dev <hoanggphann2409@gmail.com>
2026-01-12 12:28:35 +0700

    Update 007.py

--- 001-010/007.py
+++ 001-010/007.py
@@ -1,13 +1,10 @@
-def Kiem_tra_nam(year):
-  if year % 400 ==0 or year %4 ==0 and year % 100 !=0:
-    return "Đây là năm nhuận"
-  else:
-    return "Đây không phải là năm nhuận"
+def is_leap_year(year):
+  return (year % 4==0 and year % 100 !=0) or ( year % 400==0)
 try:
-  year = int(input("Nhập năm:"))
-  if year <0:
-    print("Vui lòng nhập năm sau công nguyên")
-  result=Kiem_tra_nam(year)
-  print(result)
+  year = int(input("Nhập năm cần kiểm tra:"))
+  if is_leap_year(year):
+    print(f"{year} Đây là năm nhuận")
+  else:
+    print(f"{year} Đây không phải năm nhuận")
 except ValueError:
-  print("Vui lòng nhập đúng giá trị cần kiểm tra")
+  print("Vui lòng nhập giá trị hợp lệ")
