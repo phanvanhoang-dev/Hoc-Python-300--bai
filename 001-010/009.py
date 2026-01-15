694fe71ef5eed0f6ceac55144621bb3e9266336d
phanvanhoang-dev <hoanggphann2409@gmail.com>
2026-01-15 23:17:15 +0700

    Update 009.py

--- 001-010/009.py
+++ 001-010/009.py
@@ -1,1 +1,13 @@
-
+def is_prime(n):
+    if n <=1:
+        return False
+    for i in range(2, int(n**0.5) +1):
+        if n % i == 0:
+            return False
+    else:
+        return True
+def print_prime_up_to_100():
+    for number in range(1, 101):
+        if is_prime(number):
+            print(number, end=', ')
+print_prime_up_to_100()
