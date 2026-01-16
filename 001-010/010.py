50a3259da72284d182f9f3b1f266ffdcdb3a6d4d
phanvanhoang-dev <hoanggphann2409@gmail.com>
2026-01-16 23:58:59 +0700

    Update 010.py

--- 001-010/010.py
+++ 001-010/010.py
@@ -1,1 +1,18 @@
+#import math
+#a,b = map(int,input().split())
+#print(math.gcd(a,b))
+#print(math.lcm(a,b))
+#def gcd(a, b):
+#    return a if b == 0 else gcd(b, a % b)
 
+def gcd(a,b):
+  while b!=0:
+    a,b = b, a%b
+  return a
+try:
+  no1 = input("Nhập số thứ nhất:")
+  no2 = input("Nhập số thứ hai:")
+  result = gcd(int(no1),int(no2))
+  print(f"UCLN của {no1} và {no2} là : {result} ")
+except:
+  print("Vui lòng nhập đúng giá trị")
